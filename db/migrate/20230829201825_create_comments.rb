d567f22fc950e2c191451bae722ab3bba9a73260
kayjee <kagiso.killagram@gmail.com>
2023-08-31 18:18:17 +0200

    modified migration code

--- db/migrate/20230829201825_create_comments.rb
+++ db/migrate/20230829201825_create_comments.rb
@@ -29,3 +29,30 @@ class CreateComments < ActiveRecord::Migration[7.0]
     add_index :comments, :post_id
   end
 end
+
+# Additional Migration for Creating Likes Table
+class CreateLikes < ActiveRecord::Migration[7.0]
+  def change
+    # This block creates a new database table named 'likes'.
+    create_table :likes do |t|
+      # This line establishes a foreign key relationship.
+      # It associates the 'user_id' column with the 'users' table.
+      # The 'class_name' option specifies the related class, which is 'User'.
+      # 'foreign_key: true' adds a foreign key constraint.
+      t.belongs_to :user, class_name: 'User', foreign_key: true
+      
+      # This line establishes a foreign key relationship.
+      # It associates the 'post_id' column with the 'posts' table.
+      # 'foreign_key: true' adds a foreign key constraint.
+      t.belongs_to :post, foreign_key: true
+      
+      # These lines add timestamps columns 'created_at' and 'updated_at'
+      # which automatically track when records are created and updated.
+      t.timestamps
+    end
+    
+    # Add indexes to the foreign key columns for better performance
+    add_index :likes, :user_id
+    add_index :likes, :post_id
+  end
+end
