366970a062cfcd2ae4811e43799f0a916c58f0b2
kayjee <kagiso.killagram@gmail.com>
2023-08-31 20:17:12 +0200

    fixed linters

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This class represents the main application module for KayjeeePostgresqlBlogapp.
 module ApplicationHelper
 end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # ApplicationRecord is the base class for all models in the application.
 # It inherits from ActiveRecord::Base and provides common functionality
 # that should be shared among all models.
