ab281a99cc9af611e1366db2332d7a339a96cdab
kayjee <kagiso.killagram@gmail.com>
2023-08-31 20:21:24 +0200

    fixed linters

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # ApplicationMailer is the base class for all mailers in the application.
 class ApplicationMailer < ActionMailer::Base
   default from: 'from@example.com'
