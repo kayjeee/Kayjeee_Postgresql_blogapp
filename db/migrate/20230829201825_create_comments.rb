c92a073ae572acd310be82002c4c3641fb7598a0
kayjee <kagiso.killagram@gmail.com>
2023-09-01 10:03:59 +0200

    Creadted Likes.rb

--- db/migrate/20230829201825_create_comments.rb
+++ db/migrate/20230829201825_create_comments.rb
@@ -1,61 +1,25 @@
-# frozen_string_literal: true
-
 # This migration creates the 'comments' table in the database.
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
     # This block creates a new database table named 'comments'.
     create_table :comments do |t|
-      # This line establishes a foreign key relationship.
-      # It associates the 'author_id' column with the 'users' table.
-      # The 'class_name' option specifies the related class, which is 'User'.
-      # 'foreign_key: true' adds a foreign key constraint.
-      t.belongs_to :author, class_name: 'User', foreign_key: true
+      # This line defines a foreign key reference to the 'users' table.
+      # The 'author' column is associated with the 'users' table.
+      t.references :author, foreign_key: { to_table: :users }
 
-      # This line establishes a foreign key relationship.
-      # It associates the 'post_id' column with the 'posts' table.
-      # 'foreign_key: true' adds a foreign key constraint.
-      t.belongs_to :post, foreign_key: true
+      # This line defines a foreign key reference to the 'posts' table.
+      # The 'post' column is associated with the 'posts' table.
+      t.references :post, foreign_key: { to_table: :posts }
 
       # This line defines the column for the content of the comment.
       t.text :text
 
-      # This line defines the column for the creation timestamp of the comment.
-      t.datetime :created_at
-
       # These lines add timestamps columns 'created_at' and 'updated_at'
       # which automatically track when records are created and updated.
       t.timestamps
     end
 
-    # Add indexes to the foreign key columns for better performance
-    add_index :comments, :author_id
-    add_index :comments, :post_id
-  end
-end
-
-# Additional Migration for Creating Likes Table
-class CreateLikes < ActiveRecord::Migration[7.0]
-  def change
-    # This block creates a new database table named 'likes'.
-    create_table :likes do |t|
-      # This line establishes a foreign key relationship.
-      # It associates the 'user_id' column with the 'users' table.
-      # The 'class_name' option specifies the related class, which is 'User'.
-      # 'foreign_key: true' adds a foreign key constraint.
-      t.belongs_to :user, class_name: 'User', foreign_key: true
-
-      # This line establishes a foreign key relationship.
-      # It associates the 'post_id' column with the 'posts' table.
-      # 'foreign_key: true' adds a foreign key constraint.
-      t.belongs_to :post, foreign_key: true
-
-      # These lines add timestamps columns 'created_at' and 'updated_at'
-      # which automatically track when records are created and updated.
-      t.timestamps
-    end
-
-    # Add indexes to the foreign key columns for better performance
-    add_index :likes, :user_id
-    add_index :likes, :post_id
+    # Add a composite index on [:author_id, :post_id] for better performance.
+    add_index :comments, [:author_id, :post_id]
   end
 end
