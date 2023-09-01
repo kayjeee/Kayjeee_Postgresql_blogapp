e10d32e823c2e594123698b783cb92842365963d
kayjee <kagiso.killagram@gmail.com>
2023-09-01 18:31:04 +0200

    added additional methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
+  # Associations
   has_many :posts, foreign_key: :author_id
   has_many :comments, foreign_key: :author_id
   has_many :likes, foreign_key: :author_id
 
+  # Method to get the most recent posts for a user
   def recent_posts(limit = 3)
     posts.order(created_at: :desc).limit(limit)
   end
