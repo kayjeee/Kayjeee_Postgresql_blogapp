ad007721a7d67fdb8684ff3a11d22c3348cee769
kayjee <kagiso.killagram@gmail.com>
2023-09-02 12:20:34 +0200

    added maximum legth  of 250 post

--- db/migrate/20230829200137_create_posts.rb
+++ db/migrate/20230829200137_create_posts.rb
@@ -12,18 +12,19 @@ class CreatePosts < ActiveRecord::Migration[7.0]
       t.belongs_to :author, class_name: 'User', foreign_key: true
 
       # These lines define the columns of the 'posts' table.
-      t.string :title           # Column for the post's title
+      t.string :title, limit: 250  # Column for the post's title with a maximum length of 250 characters
       t.string :photo           # Column for the URL of the post's photo
       t.text :text              # Column for the content of the post
-      t.integer :likes_counter, default: 0 # Column to count post likes
-      t.integer :comments_counter, default: 0 # Column to count post comments
+      t.integer :likes_counter, default: 0 # Column to count post likes, initialized with 0
+      t.integer :comments_counter, default: 0 # Column to count post comments, initialized with 0
 
       # These lines add timestamps columns 'created_at' and 'updated_at'
       # which automatically track when records are created and updated.
       t.timestamps
     end
 
-    # Add an index to the foreign key column 'author_id'
+    # Add an index to the foreign key column 'author_id' for better performance
     add_index :posts, :author_id
   end
 end
+
