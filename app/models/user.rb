127451864700a39a7c421ce35bfbb9ee1e927c20
kayjee <kagiso.killagram@gmail.com>
2023-09-02 09:17:46 +0200

    added validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ApplicationRecord
-  # Associations
   has_many :posts, foreign_key: :author_id
   has_many :comments, foreign_key: :author_id
   has_many :likes, foreign_key: :author_id
 
-  # Method to get the most recent posts for a user
+  # Validations for the User model
+  validates :name, presence: true
+  validates :posts_count, numericality: { greater_than_or_equal_to: 0 }
+
   def recent_posts(limit = 3)
     posts.order(created_at: :desc).limit(limit)
   end
